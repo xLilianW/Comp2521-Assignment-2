b2a2625de681286e791394e33758dcca559d97cc
Lilian Wang <lilian.wang1712@gmail.com>
2018-10-14 18:45:45 +1100

    a

---
+++ graph.c
@@ -0,0 +1,93 @@
+// Base code from week 9 Lab, editted for assignment
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "Graph.h"
+#include "Queue.h"
+
+#define TRUE    1
+#define FALSE   0
+
+// graph representation (adjacency matrix)
+typedef struct GraphRep {
+	int    nV;          // #URLs
+	int  **connections; // adjacency matrix of outgoing links
+} GraphRep;
+
+// make an edge
+Edge mkEdge(Graph g, Vertex v, Vertex w)
+{
+	assert(g != NULL && validV(g,v) && validV(g,w));
+	Edge new = {v,w}; // struct assignment
+	return new;
+}
+
+// insert an Edge
+// - sets (v,w) and (w,v)
+void insertEdge(Graph g, Vertex v, Vertex w, int wt)
+{
+	assert(g != NULL && validV(g,v) && validV(g,w));
+	if (g->edges[v][w] == 0) {
+		g->edges[v][w] = wt;
+		g->edges[w][v] = wt;
+		g->nE++;
+	}
+}
+
+// remove an Edge
+// - unsets (v,w) and (w,v)
+void removeEdge(Graph g, Vertex v, Vertex w)
+{
+	assert(g != NULL && validV(g,v) && validV(g,w));
+	if (g->edges[v][w] != 0) {
+		g->edges[v][w] = 0;
+		g->edges[w][v] = 0;
+		g->nE--;
+	}
+}
+
+// create an empty graph
+Graph newGraph(int nV)
+{
+	assert(nV > 0);
+	int v, w;
+	Graph new = malloc(sizeof(GraphRep));
+	assert(new != 0);
+	new->nV = nV; new->nE = 0;
+	new->edges = malloc(nV*sizeof(int *));
+	assert(new->edges != 0);
+	for (v = 0; v < nV; v++) {
+		new->edges[v] = malloc(nV*sizeof(int));
+		assert(new->edges[v] != 0);
+		for (w = 0; w < nV; w++)
+			new->edges[v][w] = 0;
+	}
+	return new;
+}
+
+// free memory associated with graph
+void dropGraph(Graph g)
+{
+	assert(g != NULL);
+	// not needed for this lab
+}
+
+// display graph, using names for vertices
+void showGraph(Graph g, char **names)
+{
+	assert(g != NULL);
+	printf("#vertices=%d, #edges=%d\n\n",g->nV,g->nE);
+	int v, w;
+	for (v = 0; v < g->nV; v++) {
+		printf("%d %s\n",v,names[v]);
+		for (w = 0; w < g->nV; w++) {
+			if (g->edges[v][w]) {
+				printf("\t%s (%d)\n",names[w],g->edges[v][w]);
+			}
+		}
+		printf("\n");
+	}
+}
+
--- graph.h
+++ graph.h
@@ -1,7 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
+// Base code from week 9 Lab, editted for assignment
 
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdio.h>
+
+// graph representation is hidden
 typedef struct GraphRep *Graph;
 
+// operations on graphs
+Graph newGraph(int nV);
+void dropGraph(Graph);
+Graph makeGraph(int, int**);
+void showGraph(Graph, char **);
+
+
+#endif
