aaa3aeae81fc3530a14653bdc0173fe92a552c1e
Lucas Masina <lucas.masina1@gmail.com>
2018-10-17 20:58:00 +1100

    search engine progress

--- searchPagerank.c
+++ searchPagerank.c
@@ -1,5 +1,20 @@
 
 
+struct listNode {
+    char *URL;
+    double pageWeight;
+    SearchTerm next;
+} listNode;
+
+// graph representation 
+typedef struct GraphRep {
+	int    nV;          // #URLs
+	SearchTerm  *SearchList; // list representation of outgoing links
+} GraphRep;
+
+typedef struct GraphRep *Graph;
+typedef struct listNode *SearchTerm;
+
 void main(int argc, char *argv[]){
     if(argc < 2){
         printf("Usage: searchPagerank ---search_terms---\n");
@@ -7,16 +22,33 @@ void main(int argc, char *argv[]){
     FILE *invertedIndex = fopen("invertedIndex.txt", "r");
     FILE *pagerankList = fopen("pagerankList.txt", "r");
     
-    char *searchTerm;
-    char *urlList;
-    fscanf(invertedIndex, "%s %s", searchTerm, urlList);
-    while(invertedIndex != EOF){    // Loops through entire file
-        //need to find out how to store all urls and 
-        //maintain how many search terms appear for each url
-        
-        fscanf(invertedIndex, "%s %s", searchTerm, urlList);        
+    int i = 0;
+    for(i = 0; i < argc - 1; i++){    // Create list of URLs matching each search term
+        g->nV++;
+        SearchTerm searchList = makeURLList(invertedIndex, g->SearchList[i], argv[i]);
     }
+    
     
     fclose("invertedIndex.txt");
     fclose("pagerankList.txt");
+}
+
+SearchTerm makeURLList(FILE *invertedIndex, SearchTerm s, char *sTerm){
+    char *searchTerm;
+    char *urlList;
+    
+    // Loops through entire file and finds URLs containing search term
+    fscanf(invertedIndex, "%s  %s", searchTerm, urlList);
+    while((invertedIndex != EOF) && (strcmp(searchTerm, sTerm) != 0)){    
+        //need to find out how to store all urls and 
+        //maintain how many search terms appear for each url
+        fscanf(invertedIndex, "%s  %s", searchTerm, urlList);        
+    }
+    
+    // Extract each URL and place into linked list
+    while(urlList != NULL){    
+        s->URL = strdup(strtok(urlList, " "));
+        //include page weight?
+        s = s->next;
+    }
 }
