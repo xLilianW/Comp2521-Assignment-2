efe2129208537f5f7cef04cb24b47d8549b49742
Lucas Masina <lucas.masina1@gmail.com>
2018-10-18 21:12:30 +1100

    skeleton for search engine done

--- searchPagerank.c
+++ searchPagerank.c
@@ -1,54 +1,80 @@
 
+#define TRUE    1
+#define FALSE   0
 
 struct listNode {
     char *URL;
     double pageWeight;
-    SearchTerm next;
+    int countTerms;
+    URL next;
 } listNode;
 
 // graph representation 
 typedef struct GraphRep {
-	int    nV;          // #URLs
-	SearchTerm  *SearchList; // list representation of outgoing links
+	int    nV;          // #URLs (maybe not necessary)
+	URL  SearchList; // list representation of outgoing links
 } GraphRep;
 
 typedef struct GraphRep *Graph;
-typedef struct listNode *SearchTerm;
+typedef struct listNode *URL;
 
 void main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("Usage: searchPagerank ---search_terms---\n");
+    URL URLList;
+    
+    // Make list of URLs based on number of search terms in each URL
+    int i;
+    for(i = 0; i < argc - 1; i++){
+        URLList = makeURLList(URLList, argv[i]);
     }
-    FILE *invertedIndex = fopen("invertedIndex.txt", "r");
-    FILE *pagerankList = fopen("pagerankList.txt", "r");
+    
+}
+
+// Makes list of URLs if they contain search term
+// Places URLs with more search terms closer to start
+// Places URLs with higher pageWeight closer to start
+URL makeURLList(URL list, char *searchTerm){
+    URL *URLArray = sTermURLs(searchTerm);
+    
+    //check if list is empty
+    URL start = list;
+    URL curr = list;    //should be last node of list
     
     int i = 0;
-    for(i = 0; i < argc - 1; i++){    // Create list of URLs matching each search term
-        g->nV++;
-        SearchTerm searchList = makeURLList(invertedIndex, g->SearchList[i], argv[i]);
+    while(URLArray[i] != NULL){
+        if(URLInList(list, URLArray[i])){   // Moves existing URL to front of list
+            //move to front, removing duplicate
+            //move should account for pageweight too
+            
+            URLArray[i]->countTerms++;
+        }else{    // Adds new URL to end of list
+            URLArray[i]->countTerms = 1;
+            curr->next = URLArray[i];
+            curr = curr->next;
+        }
     }
+    return start;
+}
+
+// Returns array of URLs matching search term
+char *sTermURLs(char *searchTerm){
+    FILE *invertedIndex = fopen("invertedIndex.txt", "r");
+    
+    // Loops through entire file and finds search term
+    
+    // Extract each URL matching search term and place into array
     
     
     fclose("invertedIndex.txt");
-    fclose("pagerankList.txt");
 }
 
-SearchTerm makeURLList(FILE *invertedIndex, SearchTerm s, char *sTerm){
-    char *searchTerm;
-    char *urlList;
-    
-    // Loops through entire file and finds URLs containing search term
-    fscanf(invertedIndex, "%s  %s", searchTerm, urlList);
-    while((invertedIndex != EOF) && (strcmp(searchTerm, sTerm) != 0)){    
-        //need to find out how to store all urls and 
-        //maintain how many search terms appear for each url
-        fscanf(invertedIndex, "%s  %s", searchTerm, urlList);        
+int URLInList(URL list, URL u){
+    URL curr = list;
+    while(curr != NULL){
+        if(strcmp(curr->URL, u) == 0){
+            return TRUE;
+        }
+        curr = curr->next;
     }
-    
-    // Extract each URL and place into linked list
-    while(urlList != NULL){    
-        s->URL = strdup(strtok(urlList, " "));
-        //include page weight?
-        s = s->next;
-    }
+    return FALSE;
 }
+
