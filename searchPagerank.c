01c30ce407e3c01134cf8a802b6f1701c57001da
Lucas Masina <lucas.masina1@gmail.com>
2018-10-18 22:03:48 +1100

    added more stuff to search engine

--- searchPagerank.c
+++ searchPagerank.c
@@ -46,7 +46,7 @@ URL makeURLList(URL list, char *searchTerm){
             //move should account for pageweight too
             
             URLArray[i]->countTerms++;
-        }else{    // Adds new URL to end of list
+        }else{    // Adds new URL to end of list //TODO include pageWeight
             URLArray[i]->countTerms = 1;
             curr->next = URLArray[i];
             curr = curr->next;
@@ -58,15 +58,32 @@ URL makeURLList(URL list, char *searchTerm){
 // Returns array of URLs matching search term
 char *sTermURLs(char *searchTerm){
     FILE *invertedIndex = fopen("invertedIndex.txt", "r");
+    char fileLine[BUF_SIZ];
     
     // Loops through entire file and finds search term
+    int found = FALSE;
+    while(fgets(fileLine, BUF_SIZ, invertedIndex) != NULL && found == FALSE){
+        if(strstr(fileLine, searchTerm) != NULL){
+            fileLine = strtok(fileLine, " ");    // Removes search term leaving url list
+            found = TRUE;
+        }
+    }
     
     // Extract each URL matching search term and place into array
-    
+    char *urlToken = strtok(fileLine, " ");
+    char **urlArray = malloc(strlen(urlToken));    //need calloc?
+    int i = 0;
+    while(urlToken != NULL){
+        //need to resize array?
+        urlArray[i] = strdup(urlToken);
+        urlToken = strtok(fileLine, " ");
+        i++;
+    }
     
     fclose("invertedIndex.txt");
 }
 
+// Returns TRUE if url is in list
 int URLInList(URL list, URL u){
     URL curr = list;
     while(curr != NULL){
