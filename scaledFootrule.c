bd4723053e8e4a70ce9790ac020e38534a224727
Lucas Masina <lucas.masina1@gmail.com>
2018-10-24 11:39:07 +1100

    blah

--- scaledFootrule.c
+++ scaledFootrule.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char *argv[]){
     ListNode *tLists[argc-1] = getTLists(argc-1, argv);
-    int *cList = getCList(tLists);
-    int *pList = getCList(tLists);
+    char **cList = getCList(tLists);
+    char **pList = getCList(tLists);
     
     double totalDist = 0.0;
     double minDist = -1.0;
-    int *bestPList;
+    char **bestPList;
     
     int i = 0;
-    while(i < findCard(cList)){    // Loops through each alternate set of p
+    while(i < findCard(cList)){    // Loops through each alternate set of p(TODO 1 too many loops?)
         pList = generateP(pList, i);
         int j = 0;
         while(j < findCard(cList)){    // Loops through each element in C list
             int k;
             for(k = 0; k < argc-1; k++){    // Loops through each list
-                totalDist += calcSFDist(tLists[k], cList[j], pList[j], findCard(cList));
+                totalDist += calcSFDist(tLists[k], positionInList(cList[j], cList), pList[j], findCard(cList));
                 if(totalDist < minDist || minDist == -1.0){
                     minDist = totalDist;
                     bestPList = pList;
@@ -29,15 +29,26 @@ int main(int argc, char *argv[]){
         }
         i++;
     }
+    printResult(bestPList, minDist, cList);
 }
 
-double calcSFDist(list t, int c, int p, int n){
+// Returns new list by swapping adjacent elements in list
+int *generateP(list pList, int i){
+    int temp = pList[i];
+    pList[i] = pList[i+1];
+    pList[i+1] = temp;
+    return pList;
+}
+
+// Returns calulated scaled footrule aggregation
+double calcSFDist(list t, char *c, int p, int n){
     int cardT = findCard(t);
     int cPosition = positionInList(c, t);
     
     return fabs(cPosition/cardT - p/n);    //TODO typecast to double
 }
 
+// Returns cardinality of a list
 int findCard(list L){
     int card = 0;
     while(L[i] != NULL){
@@ -46,14 +57,35 @@ int findCard(list L){
     return card;
 }
 
-int positionInList(int c, list L){
+// Returns position of string in list
+int positionInList(char *c, list L){
     int position;
     for(position = 0; position < findCard(L); position++){
-        if(L[position] == c){
+        if(strcmp(L[position], c) == 0){
             return position;
         }
     }
     return -1;
+}
+
+// Returns union of urls in lists
+char **getCList(ListNode *Lists){
+    char **list;    //malloc?
+    int j = 0;
+    
+    int i = 0;
+    while(Lists[i] != NULL){    // Loops through each list
+        ListNode curr = Lists[i];
+        while(curr != NULL){    // Loops through each list node
+            if(positionInList(curr->url, list) == -1){
+                list[j] = strdup(curr->url);    // Adds url to list
+                j++;
+            }
+            curr = curr->next;
+        }
+        i++;
+    }
+    return list;
 }
 
 // get list of ordered urls in each search list
@@ -89,4 +121,14 @@ listNode **getTLists(int nFiles, char *files[]){
         fclose(file);
     }
     return tLists;   
+}
+
+// Prints minimum distance and ranking
+void printResult(list L, double dist, list c){
+    printf("%lf\n", dist);
+    int i = 0;
+    while(i < findCard(L)){
+        printf("%s\n", c(L[i]));
+        i++;
+    }
 }
